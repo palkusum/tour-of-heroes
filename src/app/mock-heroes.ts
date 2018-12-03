e0cff66d7f0dcbb7d180dbae7e247ccc630c2763
Kusum Pal <kusump@mrinq.com>
2018-12-03 17:32:40 +0530

    created app components

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 1, name: 'Mr. Nice' },
+    { id: 2, name: 'Nacro' },
+    { id: 3, name: 'Bombasto' },
+    { id: 4, name: 'Celeritas' },
+    { id: 5, name: 'Magenta' }
+]
